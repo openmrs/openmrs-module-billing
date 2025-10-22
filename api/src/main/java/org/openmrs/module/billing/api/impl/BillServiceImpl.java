67e528f365ee3e841eea281c103c354a71a3716c
abertnamanya <abertnamanya@gmail.com>
2025-10-22 10:02:32 +0300

    Update api/src/main/java/org/openmrs/module/billing/api/impl/BillServiceImpl.java
    Co-authored-by: Nethmi Rodrigo <nethmi@openmrs.org>

--- api/src/main/java/org/openmrs/module/billing/api/impl/BillServiceImpl.java
+++ api/src/main/java/org/openmrs/module/billing/api/impl/BillServiceImpl.java
@@ -267,7 +267,7 @@ public class BillServiceImpl extends BaseEntityDataServiceImpl<Bill> implements 
 		        .concat(patient.getFamilyName() != null ? bill.getPatient().getFamilyName() : "").concat(" ")
 		        .concat(patient.getMiddleName() != null ? bill.getPatient().getMiddleName() : "");
 		String gender = patient.getGender() != null ? patient.getGender() : "";
-		String dob = bill.getPatient().getBirthdate() != null
+		String dob = patient.getBirthdate() != null
 		        ? Utils.getSimpleDateFormat("dd-MMM-yyyy").format(patient.getBirthdate())
 		        : "";
 		
