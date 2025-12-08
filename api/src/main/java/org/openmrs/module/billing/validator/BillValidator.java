47427cebd6de5c096ecb99770aa5d0f75087608b
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-12-08 19:41:25 +0530

    Fix the Hibernate cache issue with BillValidator

--- api/src/main/java/org/openmrs/module/billing/validator/BillValidator.java
+++ api/src/main/java/org/openmrs/module/billing/validator/BillValidator.java
@@ -5,6 +5,8 @@ import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.billing.api.BillService;
 import org.openmrs.module.billing.api.model.Bill;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -17,6 +19,7 @@ public class BillValidator implements Validator {
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void validate(Object target, Errors errors) {
 		if (!(target instanceof Bill)) {
 			errors.reject("error.general");
@@ -28,7 +31,7 @@ public class BillValidator implements Validator {
 			}
 			
 			if (bill.getId() != null) {
-				Bill existingBill = Context.getService(BillService.class).getBill(bill.getId());
+				Bill existingBill = Context.getService(BillService.class).getBill(bill.getBillId());
 				if (existingBill != null && !existingBill.editable()) {
 					errors.reject("billing.bill.notEditable",
 					    "Bill can only be modified when the bill is in PENDING state. Current status: "
