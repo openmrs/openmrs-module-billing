84cbd85e6a22c08176876f67553e5a78d305b6a2
NethmiRodrigo <nethmi.dilinika@gmail.com>
2026-01-26 20:26:00 +0530

    Update representation of cashpoint

--- omod/src/main/java/org/openmrs/module/billing/web/rest/resource/CashPointResource.java
+++ omod/src/main/java/org/openmrs/module/billing/web/rest/resource/CashPointResource.java
@@ -23,6 +23,7 @@ import org.openmrs.module.billing.api.model.CashPoint;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.CustomRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
@@ -41,8 +42,17 @@ public class CashPointResource extends MetadataDelegatingCrudResource<CashPoint>
 
     @Override
     public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-        DelegatingResourceDescription description = super.getRepresentationDescription(rep);
+        DelegatingResourceDescription description = new DelegatingResourceDescription();
+        description.addProperty("uuid");
+        description.addProperty("name");
+        description.addProperty("description");
+        description.addProperty("retired");
         description.addProperty("location", Representation.REF);
+        if (rep instanceof CustomRepresentation) {
+            // For custom representation, must be null
+            // - let the user decide which properties should be included in the response
+            return null;
+        }
         return description;
     }
 
