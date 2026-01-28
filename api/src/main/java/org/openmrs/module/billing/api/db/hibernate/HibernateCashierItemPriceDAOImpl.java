710fc3957bb1f240c8a15350db82f705bdb01ec8
Wikum Weerakutti <wikum@linuxdeveloper.space>
2026-01-28 10:59:14 +0530

    Remove Transactional from HibernateCashierItemPriceDAOImpl

--- api/src/main/java/org/openmrs/module/billing/api/db/hibernate/HibernateCashierItemPriceDAOImpl.java
+++ api/src/main/java/org/openmrs/module/billing/api/db/hibernate/HibernateCashierItemPriceDAOImpl.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.SessionFactory;
 import org.openmrs.module.billing.api.db.CashierItemPriceDAO;
 import org.openmrs.module.billing.api.model.CashierItemPrice;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -26,14 +25,12 @@ public class HibernateCashierItemPriceDAOImpl implements CashierItemPriceDAO {
 	
 	/** {@inheritDoc} */
 	@Override
-	@Transactional(readOnly = true)
 	public CashierItemPrice getCashierItemPrice(@Nonnull Integer id) {
 		return sessionFactory.getCurrentSession().get(CashierItemPrice.class, id);
 	}
 	
 	/** {@inheritDoc} */
 	@Override
-	@Transactional(readOnly = true)
 	public CashierItemPrice getCashierItemPriceByUuid(@Nonnull String uuid) {
 		return sessionFactory.getCurrentSession()
 		        .createQuery("from CashierItemPrice where uuid = :uuid", CashierItemPrice.class).setParameter("uuid", uuid)
@@ -42,7 +39,6 @@ public class HibernateCashierItemPriceDAOImpl implements CashierItemPriceDAO {
 	
 	/** {@inheritDoc} */
 	@Override
-	@Transactional(readOnly = true)
 	public List<CashierItemPrice> getCashierItemPrices(boolean includeRetired) {
 		return sessionFactory.getCurrentSession().createQuery(
 		    "from CashierItemPrice" + (includeRetired ? "" : " where retired = false"), CashierItemPrice.class).list();
@@ -50,7 +46,6 @@ public class HibernateCashierItemPriceDAOImpl implements CashierItemPriceDAO {
 	
 	/** {@inheritDoc} */
 	@Override
-	@Transactional
 	public CashierItemPrice saveCashierItemPrice(@Nonnull CashierItemPrice cashierItemPrice) {
 		sessionFactory.getCurrentSession().saveOrUpdate(cashierItemPrice);
 		return cashierItemPrice;
@@ -58,7 +53,6 @@ public class HibernateCashierItemPriceDAOImpl implements CashierItemPriceDAO {
 	
 	/** {@inheritDoc} */
 	@Override
-	@Transactional
 	public void purgeCashierItemPrice(@Nonnull CashierItemPrice cashierItemPrice) {
 		sessionFactory.getCurrentSession().delete(cashierItemPrice);
 	}
