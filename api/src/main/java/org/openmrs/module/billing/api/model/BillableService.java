5547ac928ad06bb41130aa00826bfa2bb59b6acf
Wikum Weerakutti <wikum@linuxdeveloper.space>
2026-09-22 22:51:22 +0530

    Change BillableService id from int to Integer (#195)
    * Change BillableService id from int to Integer
    
     Change BillableService.billableServiceId from primitive int to Integer so getId() returns null for unsaved instances. Also, cleanup the  handwritten getters and setters with Lombok
    
     Removed addServicePrice as it is not getting used anywhere.
    
    * Apply spotless
    
    * Apply spotless
    
    * Remove @Getter(AccessLevel.NONE) from billableServiceId

--- api/src/main/java/org/openmrs/module/billing/api/model/BillableService.java
+++ api/src/main/java/org/openmrs/module/billing/api/model/BillableService.java
@@ -9,17 +9,22 @@
  */
 package org.openmrs.module.billing.api.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.openmrs.BaseChangeableOpenmrsMetadata;
 import org.openmrs.Concept;
 
+@Getter
+@Setter
 public class BillableService extends BaseChangeableOpenmrsMetadata {
 	
 	private static final long serialVersionUID = 0L;
 	
-	private int billableServiceId;
+	@Setter(AccessLevel.NONE)
+	private Integer billableServiceId;
 	
 	private String name;
 	
@@ -35,90 +40,14 @@ public class BillableService extends BaseChangeableOpenmrsMetadata {
 	
 	private BillableServiceStatus serviceStatus = BillableServiceStatus.ENABLED;
 	
-	public int getBillableServiceId() {
+	@Override
+	public Integer getId() {
 		return billableServiceId;
 	}
 	
-	public void setBillableServiceId(int billableServiceId) {
+	@Override
+	public void setId(Integer billableServiceId) {
 		this.billableServiceId = billableServiceId;
 	}
 	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getShortName() {
-		return shortName;
-	}
-	
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
-	
-	public Concept getServiceType() {
-		return serviceType;
-	}
-	
-	public void setServiceType(Concept serviceType) {
-		this.serviceType = serviceType;
-	}
-	
-	public Concept getServiceCategory() {
-		return serviceCategory;
-	}
-	
-	public void setServiceCategory(Concept serviceCategory) {
-		this.serviceCategory = serviceCategory;
-	}
-	
-	public BillableServiceStatus getServiceStatus() {
-		return serviceStatus;
-	}
-	
-	public void setServiceStatus(BillableServiceStatus serviceStatus) {
-		this.serviceStatus = serviceStatus;
-	}
-	
-	@Override
-	public Integer getId() {
-		return getBillableServiceId();
-	}
-	
-	@Override
-	public void setId(Integer integer) {
-		setBillableServiceId(integer);
-	}
-	
-	public List<CashierItemPrice> getServicePrices() {
-		return servicePrices;
-	}
-	
-	public void setServicePrices(List<CashierItemPrice> servicePrices) {
-		this.servicePrices = servicePrices;
-	}
-	
-	public Concept getConcept() {
-		return concept;
-	}
-	
-	public void setConcept(Concept concept) {
-		this.concept = concept;
-	}
-	
-	public void addServicePrice(CashierItemPrice price) {
-		if (price == null) {
-			throw new NullPointerException("Service Price must be defined.");
-		}
-		
-		if (this.servicePrices == null) {
-			this.servicePrices = new ArrayList<CashierItemPrice>();
-		}
-		
-		this.servicePrices.add(price);
-		price.setBillableService(this);
-	}
 }
